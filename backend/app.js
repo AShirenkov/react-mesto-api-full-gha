1d4436a63736c407c4dd4980da162e5189c555c7
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-08-12 09:29:33 +0300

    fix: rкоммит для теста

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-// Слушаем 3000 порт
+// Слушаем 3000  порт
 const mongoose = require("mongoose");
 
 const { errors } = require("celebrate");
